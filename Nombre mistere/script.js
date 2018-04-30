6f9b64114bde0d324b3554fd0b488ed01b431d4a
david <david.journo.31@gmail.com>
2018-04-30 21:24:55 +0200

    first commit

---
+++ Nombre mistere/script.js
@@ -0,0 +1,21 @@
+function nbMistere (min, max){
+  
+}
+var nb = Math.floor(Math.random()*9)
+var result = (nb < ecrire ) ? "Plus petit" : "Plus grand"
+var ecrire = ""; prompt ("Devine un nombre entre 0 et 9");
+
+console.log(nb)
+
+/* var nb = Math.floor(Math.random()*10)
+var result = (nb < ecrire ) ? "Plus petit" : "Plus grand"
+var ecrire = ""; prompt ("Devine un nombre entre 0 et 9");
+
+function compare (){
+  if(nb === ecrire){
+  return "bravo";
+} else {
+return "Essaye encore";
+}
+}
+console.log(result) */
